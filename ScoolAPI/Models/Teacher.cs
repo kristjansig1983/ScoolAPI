6c8d1cbb78085b289f6f1f8d736bcf061f0fcd8b
kristjansig1983 <kristjansig1983@gmail.com>
2023-04-11 12:59:58 +0000

    Add project files.

---
+++ ScoolAPI/Models/Teacher.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScoolAPI.Models
+{
+    public class Teacher
+    {
+        public int TeacherId { get; set; }
+        public string FirstName { get; set; }
+        [MaxLength(255)]
+        public string LastName { get; set; }
+    }
+}
---
+++ ScoolAPI/Models/TeacherController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScoolAPI.Models
+{
+    [Route("api/teachers")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        [HttpGet]
+        public List<Teacher> GetAllTeachers()
+        {
+            return new List<Teacher>() {
+                new Teacher() { TeacherId = 1, FirstName = "Hjörtur", LastName = "Pálmi"},
+                new Teacher() { TeacherId = 1, FirstName = "Adam", LastName = "Hart"},
+                new Teacher() { TeacherId = 1, FirstName = "Skúli", LastName = "Gunnsteinsson"},
+                new Teacher() { TeacherId = 1, FirstName = "Björgvin", LastName = "Páll"},
+            };
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<Teacher> GetTeacherById(int id)
+        {
+            List<Teacher> teachers = new List<Teacher>() {
+                new Teacher() { TeacherId = 1, FirstName = "Hjörtur", LastName = "Pálmi"},
+                new Teacher() { TeacherId = 2, FirstName = "Adam", LastName = "Hart"},
+                new Teacher() { TeacherId = 3, FirstName = "Skúli", LastName = "Gunnsteinsson"},
+                new Teacher() { TeacherId = 4, FirstName = "Björgvin", LastName = "Páll"},
+            };
+            foreach (Teacher teacher in teachers)
+            {
+                if (teacher.TeacherId == id)
+                {
+                    return teacher;
+                }
+            }
+            return NotFound();
+        }
+    }
+}
