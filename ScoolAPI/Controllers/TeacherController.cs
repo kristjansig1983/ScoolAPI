e93feec104fc9b7292141596e9b492d0bde34b8b
kristjansig1983 <kristjansig1983@gmail.com>
2023-04-12 09:49:53 +0000

    Made mock repository. Still working as before.

--- ScoolAPI/Controllers/TeacherController.cs
+++ ScoolAPI/Controllers/TeacherController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ScoolAPI.Data;
 using ScoolAPI.Models;
 
 namespace ScoolAPI.Controllers
@@ -8,35 +9,32 @@ namespace ScoolAPI.Controllers
     [ApiController]
     public class TeacherController : ControllerBase
     {
+        private MockRepository _repo;
+
+        public TeacherController()
+        {
+            _repo = new MockRepository();
+        }
+
         [HttpGet]
         public List<Teacher> GetAllTeachers()
         {
-            return new List<Teacher>() {
-                new Teacher() { TeacherId = 1, FirstName = "Hjörtur", LastName = "Pálmi"},
-                new Teacher() { TeacherId = 1, FirstName = "Adam", LastName = "Hart"},
-                new Teacher() { TeacherId = 1, FirstName = "Skúli", LastName = "Gunnsteinsson"},
-                new Teacher() { TeacherId = 1, FirstName = "Björgvin", LastName = "Páll"},
-            };
+            return _repo.GetAllTeachers();
         }
 
         [HttpGet]
         [Route("{id}")]
         public ActionResult<Teacher> GetTeacherById(int id)
         {
-            List<Teacher> teachers = new List<Teacher>() {
-                new Teacher() { TeacherId = 1, FirstName = "Hjörtur", LastName = "Pálmi"},
-                new Teacher() { TeacherId = 2, FirstName = "Adam", LastName = "Hart"},
-                new Teacher() { TeacherId = 3, FirstName = "Skúli", LastName = "Gunnsteinsson"},
-                new Teacher() { TeacherId = 4, FirstName = "Björgvin", LastName = "Páll"},
-            };
-            foreach (Teacher teacher in teachers)
+            
+            Teacher? teacher = _repo.GetTeacherById(id);
+
+            if (teacher == null)
             {
-                if (teacher.TeacherId == id)
-                {
-                    return teacher;
-                }
+                return NotFound();
             }
-            return NotFound();
+            return teacher;
+            
         }
     }
 }
---
+++ ScoolAPI/Data/MockRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScoolAPI.Models;
+
+namespace ScoolAPI.Data
+{
+    public class MockRepository
+    {
+        List<Teacher> Teachers = new List<Teacher>() {
+        new Teacher() { TeacherId = 1, FirstName = "Hjörtur", LastName = "Pálmi"},
+        new Teacher() { TeacherId = 1, FirstName = "Adam", LastName = "Hart"},
+        new Teacher() { TeacherId = 1, FirstName = "Skúli", LastName = "Gunnsteinsson"},
+        new Teacher() { TeacherId = 1, FirstName = "Björgvin", LastName = "Páll"},
+    };
+
+        public List<Teacher> GetAllTeachers()
+        {
+            return Teachers;
+        }
+
+        public Teacher? GetTeacherById(int id)
+        {
+
+            foreach (Teacher teacher in Teachers)
+            {
+                if (teacher.TeacherId == id)
+                {
+                    return teacher;
+                }
+            }
+            return null;
+        }
+
+    }
+}
